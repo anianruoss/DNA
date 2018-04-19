e8fbbf13e7fc126f1085e1434475677dcdab8c88
Anian Ruoss <anian.ruoss@gmail.com>
2018-04-19 21:27:04 +0200

    Implement quadtree image segmentation

--- exercise07/problem2/main.cpp
+++ exercise07/problem2/main.cpp
@@ -11,9 +11,9 @@
 #include "bitmap.hpp"
 
 
-template <typename Function>
+template<typename Function>
 Array Sum(unsigned int width, unsigned int height, Function f) {
-    Array sum (width+1, height+1);
+    Array sum(width + 1, height + 1);
 
     for (unsigned int y = 0; y <= height; ++y) {
         float sx = 0;
@@ -21,8 +21,8 @@ Array Sum(unsigned int width, unsigned int height, Function f) {
             if (x == 0 || y == 0) {
                 sum(x, y) = 0;
             } else {
-                sx += f(x-1, y-1);
-                sum(x, y) = sx + sum(x, y-1);
+                sx += f(x - 1, y - 1);
+                sum(x, y) = sx + sum(x, y - 1);
             }
         }
     }
@@ -30,45 +30,46 @@ Array Sum(unsigned int width, unsigned int height, Function f) {
     return sum;
 }
 
-double GetSum(const Array& sum, unsigned int x, unsigned int y, unsigned int w,
+double GetSum(const Array &sum, unsigned int x, unsigned int y, unsigned int w,
               unsigned int h) {
-    return sum(x+w, y+h) - sum(x+w, y) - sum(x, y+h) + sum(x,y);
+    return sum(x + w, y + h) - sum(x + w, y) - sum(x, y + h) + sum(x, y);
 }
 
-void Fill(Array& result, double value, unsigned int x, unsigned int y,
+void Fill(Array &result, double value, unsigned int x, unsigned int y,
           unsigned int w, unsigned int h) {
     for (unsigned int ox = 0; ox < w; ++ox) {
         for (unsigned int oy = 0; oy < h; ++oy) {
-            result(x+ox, y+oy) = value;
+            result(x + ox, y + oy) = value;
         }
     }
 }
 
-double Minimize(double gamma, Array& result, const Array& sum,
-                const Array& ssq, unsigned int x, unsigned int y,
+double Minimize(double gamma, Array &result, const Array &sum,
+                const Array &ssq, unsigned int x, unsigned int y,
                 unsigned int w, unsigned int h) {
     if (w == 1 || h == 1) {
-        result(x,y) = GetSum(sum, x, y, w, h);
+        result(x, y) = GetSum(sum, x, y, w, h);
         return gamma;
     }
 
     double s = GetSum(sum, x, y, w, h);
-    double e = GetSum(ssq, x, y, w, h) - (s*s / (w*h)) + gamma;
+    double e = GetSum(ssq, x, y, w, h) - (s * s / (w * h)) + gamma;
 
-    double se = Minimize(gamma, result, sum, ssq, x, y, w/2, h/2);
-    se += Minimize(gamma, result, sum, ssq, x+w/2, y, w/2, h/2);
-    se += Minimize(gamma, result, sum, ssq, x, y+h/2, w/2, h/2);
-    se += Minimize(gamma, result, sum, ssq, x+w/2, y+h/2, w/2, h/2);
+    double se = Minimize(gamma, result, sum, ssq, x, y, w / 2, h / 2);
+    se += Minimize(gamma, result, sum, ssq, x + w / 2, y, w / 2, h / 2);
+    se += Minimize(gamma, result, sum, ssq, x, y + h / 2, w / 2, h / 2);
+    se += Minimize(gamma, result, sum, ssq, x + w / 2, y + h / 2, w / 2,
+                   h / 2);
 
     if (e < se) {
-        Fill(result, s/(w*h), x, y, w, h);
+        Fill(result, s / (w * h), x, y, w, h);
         return e;
     } else {
         return se;
     }
 }
 
-void ShowResult(const Bitmap& original, const Array& result,
+void ShowResult(const Bitmap &original, const Array &result,
                 const std::string filename) {
     std::string make_dir = "mkdir -p bin/results/" + filename;
 
@@ -98,17 +99,17 @@ int main() {
     Bitmap bitmap = readpgm(file);
 
     std::cout << "input gamma (e.g. 0.01)? ";
-    double gamma=0.1;
+    double gamma = 0.1;
     std::cin >> gamma;
     Array img = BitmapToArray(bitmap);
     unsigned int width = img.Width();
     unsigned int height = img.Height();
 
     Array sum = Sum(width, height, img);
-    Array ssq = Sum(width, height, [&] (unsigned int x, unsigned int y) {
-        return img(x,y)*img(x,y);
+    Array ssq = Sum(width, height, [&](unsigned int x, unsigned int y) {
+        return img(x, y) * img(x, y);
     });
-    Array result(width,height);
+    Array result(width, height);
 
     std::cout << std::endl;
     std::cout << "minimum = "
@@ -117,6 +118,6 @@ int main() {
 
     ShowResult(bitmap, result, filename);
 
-    return  0;
+    return 0;
 }
 
